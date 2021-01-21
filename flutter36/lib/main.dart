7b354916cfc8bdcb7049e6af317841f13e441299
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-21 21:48:21 +0800

    3

--- flutter36/lib/main.dart
+++ flutter36/lib/main.dart
@@ -3,3 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('API DEMO'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Hasil Kembalian'),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('POST'),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
