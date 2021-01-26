79d0d2023328fef0b10e67457ef5052a0e6a05ed
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-26 21:44:24 +0800

    Update main.dart

--- flutter39/lib/main.dart
+++ flutter39/lib/main.dart
@@ -4,11 +4,47 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isON = false;
+  Widget myWidget
+  idget = Container(width: 200,
+  height: 100, decoration: BoxDecoration(color: Colors.red,
+  border: Border.all(color: Colors.black, width: 3)),);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Animated Switcher'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              AnimatedSwitcher(child: myWidget,
+              duration: Duration(seconds: 1),),
+              Switch(),
+                activeColor: Colors.green,
+                inactiveTrackColor: Colors.green[200],
+                value: isON,
+                onChanged: (newValue) {
+                  isON = newValue;
+                  setState(() {
+                    if(isON)
+                    myWidget = Text('Switch: ON',
+                    style: Textstyle(color weight, FonWeight: FontWeight.w700))
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
