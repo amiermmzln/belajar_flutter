240a6eb21c9bc31f710bb934159f5aa9bae3107e
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-27 14:10:47 +0800

    q

--- flutter39/lib/main.dart
+++ flutter39/lib/main.dart
@@ -11,10 +11,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool isON = false;
-  Widget myWidget
-  idget = Container(width: 200,
-  height: 100, decoration: BoxDecoration(color: Colors.red,
-  border: Border.all(color: Colors.black, width: 3)),);
+  Widget myWidget = Container(
+    width: 200,
+    height: 100,
+    decoration: BoxDecoration(
+        color: Colors.red, border: Border.all(color: Colors.black, width: 3)),
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,18 +28,33 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              AnimatedSwitcher(child: myWidget,
-              duration: Duration(seconds: 1),),
-              Switch(),
+              AnimatedSwitcher(
+                child: myWidget,
+                duration: Duration(seconds: 1),
+              ),
+              Switch(
                 activeColor: Colors.green,
                 inactiveTrackColor: Colors.green[200],
                 value: isON,
                 onChanged: (newValue) {
                   isON = newValue;
                   setState(() {
-                    if(isON)
-                    myWidget = Text('Switch: ON',
-                    style: Textstyle(color weight, FonWeight: FontWeight.w700))
+                    if (isON)
+                      myWidget = Text(
+                        'Switch: ON',
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20),
+                      );
+                    else
+                      myWidget = Container(
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            border: Border.all(color: Colors.black, width: 3)),
+                      );
                   });
                 },
               )
