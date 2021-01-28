0b95f44aae804b9861dd02fb765dde8c635c8ea0
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-28 10:28:21 +0800

    1

--- flutter41/lib/main.dart
+++ flutter41/lib/main.dart
@@ -3,3 +3,38 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController(text: 'No Name');
+  bool isON = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('shared Preferences Examples'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+              ),
+              Switch(
+                value: isON,
+                onChanged: (newValue) {
+                  isON = newValue;
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
