1cfde94ae258b9defcbb40e15176090c98b66d65
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-28 10:52:48 +0800

    Shared preferences and double question mark (??)

--- flutter41/lib/main.dart
+++ flutter41/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +13,29 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   TextEditingController controller = TextEditingController(text: 'No Name');
   bool isON = false;
+
+  void saveData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString('nama', controller.text);
+    pref.setBool('isON', isON);
+  }
+
+  Future<String> getNama() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getString('nama') ?? 'No name';
+  }
+
+  Future<bool> getON() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getBool('ison') ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('shared Preferences Examples'),
+          title: Text('Shared Preferences Examples'),
         ),
         body: Center(
           child: Column(
@@ -30,6 +48,25 @@ class _MyAppState extends State<MyApp> {
                 onChanged: (newValue) {
                   setState(() {
                     isON = newValue;
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text('SAVE'),
+                onPressed: () {
+                  saveData();
+                },
+              ),
+              RaisedButton(
+                child: Text('LOAD'),
+                onPressed: () {
+                  getNama().then((s) {
+                    controller.text = s;
+                    setState(() {});
+                  });
+                  getON().then((b) {
+                    isON = b;
+                    setState(() {});
                   });
                 },
               )
